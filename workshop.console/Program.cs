0265c14be36ff0bb6b764c2994c077f95a61e7fe
uerbzr <nigel.sibbert@gmail.com>
2025-01-08 09:01:27 +0000

    added some queries

--- workshop.console/Program.cs
+++ workshop.console/Program.cs
@@ -61,15 +61,38 @@ both.ForEach(x => Console.WriteLine(x));
 #region Queries
 
 //querying strings
-Dictionary<string, int> Fridge = new Dictionary<string, int>();
-Fridge.Add("Carrots", 5);
-Fridge.Add("Potatoes", 8);
-Fridge.Add("Peppers", 5);
-Fridge.Add("Beetroot", 4);
-Fridge.Add("Cucumber", 2);
-Fridge.Add("Broccoli", 1);
-Fridge.Add("Onions", 8);
-Fridge.Add("Shallots", 8);
+Dictionary<string, int> fridgeDictionary = new Dictionary<string, int>();
+fridgeDictionary.Add("Carrots", 5);
+fridgeDictionary.Add("Potatoes", 8);
+fridgeDictionary.Add("Peppers", 5);
+fridgeDictionary.Add("Beetroot", 4);
+fridgeDictionary.Add("Cucumber", 2);
+fridgeDictionary.Add("Broccoli", 1);
+fridgeDictionary.Add("Onions", 8);
+fridgeDictionary.Add("Shallots", 8);
+
+//lamda collection
+var lamdalowveg = fridgeDictionary.Where(x => x.Value < 5);
+
+//linq collection
+var linqlowveg = from veg in fridgeDictionary
+                 where veg.Value < 5
+                 select veg;
+
+//lamda returning single item
+var lamdaSingle = fridgeDictionary.FirstOrDefault(x => x.Key == "Carrots");
+var lamda_carrot = fridgeDictionary.Where(item => item.Key=="Carrots").FirstOrDefault();
+var lamda_danger = fridgeDictionary.First(item => item.Key == "Carrots");
+
+//lamda p items
+var lamda_p_items = fridgeDictionary.Where(item => item.Key.ToLower().StartsWith("p")).ToList();
+
+//linq p items
+var linq_p_items = from item in fridgeDictionary
+                   where item.Key.ToLower().StartsWith("p")
+                   select item;
+
+
 
 
 #endregion
